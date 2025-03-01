31617cdb6256f6b289c30cbd9a1805e70202cdba
Layth-m <laythmaloul2021@gmail.com>
2025-03-01 00:14:18 -0600

    Add ship rotation.
    WASD keys now control ship 2D rotation.

---
+++ Assets/scripts/PlayerRotate.cs
@@ -0,0 +1,32 @@
+
+
+using UnityEngine;
+
+public class PlayerRotate: MonoBehaviour
+{
+    public float rotationSpeed = 100f;
+
+    void Update()
+    {
+        float rotation = 0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rotation = rotationSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rotation = -rotationSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rotation = rotationSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rotation = -rotationSpeed * Time.deltaTime;
+        }
+
+        transform.Rotate(0, 0, rotation);
+    }
+}
